d9081b2bcb5b004f1a060b6dc0d88b6091ed88de
julius <nyerereoffice@gmail.com>
2024-05-08 04:20:03 +0300

    Registering Schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@
 
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:/1023')
+    MongooseModule.forRoot('mongodb://127.0.0.1:/1023'),
+    UsersModule
   ],
   controllers: [],
   providers: [],
